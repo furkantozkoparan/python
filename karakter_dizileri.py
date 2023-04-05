b93d5d694627a4712e815ba943657bdce9690868
furkantozkoparan <125921593+furkantozkoparan@users.noreply.github.com>
2023-04-05 16:06:02 +0300

    Update karakter_dizileri.py

--- karakter_dizileri.py
+++ karakter_dizileri.py
@@ -1,1 +1,18 @@
+website = 'http://www.sadikturan.com'
+course = 'Python Kursu: Bastan sona python programlama rehberiniz (40 saat)'
 
+# 'Course' karakter dizisinde kac karakter bulunmaktadir.
+
+# 'Website' icinde www karakterlerini alin.
+
+# 'Website' icinde com karakterlerini alin.
+
+# 'Course' icinde ilk 15 ve son 15 karakterlerini alin.
+
+# 'Course' icindeki karakterleri tersten yazdiralim.
+
+# 'name, surname, age, job = 'Bora', 'Yilmaz', 32, 'muhendis'' degişkenleri ile 'benim adım bora yılmaz, yaşım 32 ve mesleğim mühendis' ifadesini yazdirin.
+
+# 'Hello world' ifadesindeki w harfini 'W' ile degistirin.
+
+# 'abc' ifadesini yan yana 3 defa yazdirin.
