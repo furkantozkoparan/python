e632db3a517e346b94ff099f7e9862f92e144ebd
furkantozkoparan <125921593+furkantozkoparan@users.noreply.github.com>
2023-05-09 14:11:46 +0300

    Giris.py

---
+++ Giris.py
@@ -0,0 +1,13 @@
+# İnteger: Tam sayilari ifade eder. 
+# Floats: Kesirli sayilari idade eder.
+# Boolean: Bir seyin doğru ya da yanlis oldugunu ifade eder.(True-False)
+r = 2 < 3
+print(r)
+
+# Type: Veri tiplerini verir.
+r = type(2)
+print(r)
+
+# Type Casting: Objelerin veri tiplerinin donusturulmesi. (2.8 Değerine sahip olan Float haldeki verinin tipini integera dönüştürmek için kullanıldı)
+r = float(2)                                 
+print(r)
