8a468372b29b9c68fff1968b054669aa808db188
furkantozkoparan <125921593+furkantozkoparan@users.noreply.github.com>
2023-05-10 16:53:07 +0300

    Update Donguler.py

--- Donguler.py
+++ Donguler.py
@@ -22,3 +22,27 @@ While
       x += 1
       print(toplam)
   # Bu ifade ile x degeri 100'den kucuk olan sayilar toplanacaktır.
+
+For
+# while dongusunden farkli olarak for da dongunun yapilmasini istedigimiz sayisi belirtilir. Ornegin:
+  toplam = 0
+  for x in range(101):
+      toplam += x
+      print(toplam)
+  # Bu ifade ile birlikte 0'dan 100'e kadar olan sayilar toplanacaktir. While da bunu yaparken ne kadar True cikarsa topluyordu.
+  
+Break 
+# Istenilen bir kosul saglandiginda sorguyu durdurmaya yarar. Ornegin: for ile yazilmis bir seyde 10'a kadar yazdirma yapilirken 5'e gelince dur denilebilir.
+  for i in range(10):
+      if i == 3:
+          break
+      print(i)
+  # Bu sorgu ile 0'dan 9'a kadar sayilar siralanirken sayi eger 3'e esit olursa sorgu sonlanacaktir. Sonuc olarak bu bize 0, 1, 2 verir ve siradaki 3 oldugu icin sorgu sonlanir.
+  
+Continue
+# Bazi dongulerde istenilen degeri atlamaya yarar. Ornegin: bir sorguda 10'a kadar yazdirma yapilirken 5'i atlayip sorguya devam edebiliriz.
+for i in range(10):
+     if i == 3:
+        continue
+     print(i)
+# Bu sorgu ile 0'dan 9'a kadar sayilar siralanirken sayi eger siralanmaya baslayacak fakat 3'e gelince bunu dahil etmeden sorguya devam edecektir. Sonuc olarak bu bize 0, 1, 2, 4, 5... 9 verir.  
